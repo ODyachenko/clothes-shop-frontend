3b9ac9b33e3e71647641ee15edf4d681f0412eab
ODyachenko <oleg1696@gmail.com>
2024-02-21 10:04:11 +0200

    choose color logic

--- src/components/Colors/ColorsList.tsx
+++ src/components/Colors/ColorsList.tsx
@@ -1,16 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { ProductColorsType } from '../../../@types';
 import ColorsListItem from './ColorsListItem';
+import './styles.scss';
 
 type ColorsListProps = {
   colors: ProductColorsType[];
 };
 
 const ColorsList: FC<ColorsListProps> = ({ colors }) => {
+  const [selectedColorId, setSelectedColorId] = useState<number>(1);
+
+  const onClickHandler = (id: number) => {
+    setSelectedColorId(id);
+  };
+
   return (
     <ul className="colors__list flex flex-wrap gap-4">
       {colors.map((color) => (
-        <ColorsListItem key={color.id} {...color} />
+        <ColorsListItem
+          key={color.id}
+          {...color}
+          selectedColorId={selectedColorId}
+          onClickHandler={onClickHandler}
+        />
       ))}
     </ul>
   );
--- src/components/Colors/ColorsListItem.tsx
+++ src/components/Colors/ColorsListItem.tsx
@@ -1,11 +1,29 @@
 import React, { FC } from 'react';
 import { ProductColorsType } from '../../../@types';
 
-const ColorsListItem: FC<ProductColorsType> = ({ value }) => {
+type ColorsListItemProps = {
+  id: number;
+  value: string;
+  selectedColorId: number;
+  onClickHandler: (id: number) => void;
+};
+
+const ColorsListItem: FC<ColorsListItemProps> = ({
+  id,
+  value,
+  selectedColorId,
+  onClickHandler,
+}) => {
   return (
-    <li className="colors__list-item">
+    <li
+      className={`colors__list-item rounded-full border-2 border-border-10 
+      border-solid  hover:border-gray-400 ${
+        id === selectedColorId ? 'active border-gray-400' : ''
+      }`}
+    >
       <button
-        className="w-10 h-10 rounded-full bg-black border-2 border-border-10 border-solid  hover:border-gray-300"
+        onClick={() => onClickHandler(id)}
+        className={`w-10 h-10 rounded-full `}
         style={{ backgroundColor: value }}
       />
     </li>
