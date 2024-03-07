a184dc083abc031a26b211e797f79baee3ac5bd9
ODyachenko <oleg1696@gmail.com>
2024-03-07 12:12:00 +0200

    change favicons and title

--- src/components/ProfileCategories/ProfileActions.tsx
+++ src/components/ProfileCategories/ProfileActions.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ProfileActions: FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="mt-6 flex items-center justify-end gap-x-6">
       <button
+        onClick={() => navigate(-1)}
         type="button"
         className="text-sm font-semibold leading-6 text-gray-900"
       >
