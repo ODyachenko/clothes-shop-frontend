66db9df7339d080b507d5e896161e5472ed799bb
ODyachenko <oleg1696@gmail.com>
2024-03-14 17:22:05 +0200

    fix browse section size

--- src/components/Browse/Browse.tsx
+++ src/components/Browse/Browse.tsx
@@ -6,11 +6,13 @@ import './styles.scss';
 const Browse: FC = () => {
   return (
     <section className="browse">
-      <div className="browse__inner bg-search-bg py-10 px-6 rounded-3xl lg:py-16 lg:px-14">
-        <h2 className="browse__title title text-center mb-7 lg:mb-16">
-          BROWSE BY dress STYLE
-        </h2>
-        <BrowseList />
+      <div className="container">
+        <div className="browse__inner bg-search-bg py-10 px-6 rounded-3xl lg:py-16 lg:px-14">
+          <h2 className="browse__title title text-center mb-7 lg:mb-16">
+            BROWSE BY dress STYLE
+          </h2>
+          <BrowseList />
+        </div>
       </div>
     </section>
   );
